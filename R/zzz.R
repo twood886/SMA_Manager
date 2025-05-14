fe600b4ce1fe3d3e968a6bb2dbc915fbfa1d9ddb
Taylor Wood <twood886@gmail.com>
2025-05-14 15:20:17 -0400

    fixing registries for app

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
+#' The master registries environment
+#'
+#' This environment (and its sub‐environments) is created on package load.
+#' @export
+registries <- NULL
+
 .onLoad <- function(libname, pkgname) {
   # Assign the environment to the package namespace
   registries <<- new.env(parent = emptyenv())
